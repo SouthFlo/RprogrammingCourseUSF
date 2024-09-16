2df19587399634592eb0a10a39c58347105d1450
SouthFlo <lovesouthflo@gmail.com>
2024-09-16 07:50:00 -0400

    Create Dataframe_Matrix.R

---
+++ Dataframe_Matrix.R
@@ -0,0 +1,14 @@
+# Imputed variables
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+ABC_political_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
+CBS_political_poll_results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+# Creating Dataframe
+presidential_election <- data.frame(Name, ABC_political_poll_results, CBS_political_poll_results)
+View(presidential_election)
+
+# Construct presidential_election_matrix
+pres_matrix <- c(ABC_political_poll_results, CBS_political_poll_results)
+presidential_election_matrix <- matrix(pres_matrix, nrow = 2, byrow = TRUE)
+# Name columns
+colnames(presidential_election_matrix) <- Name
