bbe08ee52d77f714a6eb7f84f1158770a1f02249
SouthFlo <lovesouthflo@gmail.com>
2024-10-22 01:08:45 -0400

    Add files via upload

---
+++ Input_Output.R
@@ -0,0 +1,23 @@
+#Input/Output, string manipulation and plyr package 
+library(plyr)
+
+# Importing Data, separating elements within a string with sep = ","
+Student <- read.table("Assignment_6_Dataset.txt", header = TRUE, sep = ",")
+
+# Calculate the average grade for each gender in the Student data frame using the ddply() function
+#(use plyr package for this operation).
+Students_average = ddply(Student, 
+                       "Sex", 
+                       transform,
+                       Grade.Average = mean(Grade))
+
+# Write the resulting output to a file.
+write.table(Students_average, "Students_Gendered_Mean.csv", sep = ",")
+
+# Convert the data set to a dataframe for names whos' name contains the letter i, 
+#then create a new data set with those names.
+i_students <- subset(Student, grepl("i", Student$Name, ignore.case=T))
+
+# Write those names to a file separated by comma’s (CSV)
+write.csv(i_students,"DataSubset.csv")
+
